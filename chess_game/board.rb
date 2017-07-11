71a11905a0bff2499d1ec84c31c826e2f88080db
Jan-Luca Berger <janluca@officeconduct.com>
2017-07-11 10:12:40 -0400

    Create pieces hash. Add methods to generate pieces

--- chess_game/board.rb
+++ chess_game/board.rb
@@ -1,5 +1,73 @@
 require_relative "piece"
 
+
+
+P1_PIECES = {
+  Rook: [
+    [0, 0],
+    [0, 7]
+  ],
+  Knight: [
+    [0, 1],
+    [0, 6]
+  ],
+  Bishop: [
+    [0, 2],
+    [0, 5]
+  ],
+  Queen: [
+    [0, 3]
+  ],
+  King: [
+    [0,4]
+  ],
+  Pawn: [
+    [1, 0],
+    [1, 1],
+    [1, 2],
+    [1, 3],
+    [1, 4],
+    [1, 5],
+    [1, 6],
+    [1, 7]
+  ]
+}
+
+P2_PIECES = {
+  Rook: [
+    [7, 0],
+    [7, 7]
+  ],
+  Knight: [
+    [7, 1],
+    [7, 6]
+  ],
+  Bishop: [
+    [7, 2],
+    [7, 5]
+  ],
+  Queen: [
+    [7, 3]
+  ],
+  King: [
+    [7,4]
+  ],
+  Pawn: [
+    [7, 0],
+    [7, 1],
+    [7, 2],
+    [7, 3],
+    [7, 4],
+    [7, 5],
+    [7, 6],
+    [7, 7]
+  ]
+}
+
+ALL_PIECES = [P1_PIECES, P2_PIECES]
+
+
+
 class Board
 
   attr_accessor :board
@@ -20,6 +88,43 @@ class Board
       end
     end
   end
+
+  def parse_pieces
+   player_val = 1
+   ALL_PIECES.each do |player_pieces|
+     player_pieces.each do |piece, all_pos|
+       all_pos.each do |position|
+         @grid[position[0]][position[1]] = generate_piece(piece, player_val, position)
+       end
+     end
+     player_val += 1
+   end
+ end
+
+
+ def generate_piece(sym, player, position)
+   case sym
+   when :Rook
+     # puts "Create Rook #{player} #{position}"
+     Rook.new(player, position)
+   when :Knight
+     # puts "Create Knight #{player} #{position}"
+     Knight.new(player, position)
+   when :Bishop
+     # puts "Create Bishop #{player} #{position}"
+     Bishop.new(player, position)
+   when :Queen
+     # puts "Create Queen #{player} #{position}"
+     Queen.new(player, position)
+   when :King
+     # puts "Create King #{player} #{position}"
+     King.new(player, position)
+   when :Pawn
+     # puts "Create Pawn #{player} #{position}"
+     Pawn.new(player, position)
+   end
+ end
+ 
 
   def move_piece(start_pos, end_pos)
     raise ArgumentError.new("There is no piece at #{start_pos}") if self[start_pos].is_a?(NullPiece)
